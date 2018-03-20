93f953af2eea1fc78e48f175a8eb0c5d9d310935
Dmitry Galinsky <dima.exe@gmail.com>
2018-03-20 20:27:48 +0300

    fix extension name prefix

--- ext/r4r/ring_bits_ext/extconf.rb
+++ ext/r4r/ring_bits_ext/extconf.rb
@@ -2,6 +2,6 @@ require "mkmf"
 
 $CFLAGS = "-O2 -Wall -std=c99"
 
-extension_name = 'ring_bits_ext'
+extension_name = 'r4r/ring_bits_ext'
 dir_config(extension_name)
 create_makefile "#{extension_name}"
--- ext/r4r/system_clock_ext/extconf.rb
+++ ext/r4r/system_clock_ext/extconf.rb
@@ -2,7 +2,7 @@ require "mkmf"
 
 $CFLAGS = "-O2 -Wall -std=c99"
 
-extension_name = 'system_clock_ext'
+extension_name = 'r4r/system_clock_ext'
 dir_config(extension_name)
 create_makefile "#{extension_name}"
 
